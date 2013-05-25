bea981f95a3c33a823145ece67ec9d18d9c7a181
yuri.ketov@gmail.com <yuri.ketov@gmail.com@fe120c1f-a36c-d6e5-01d3-82b0a1d691a2>
2013-05-25 08:30:27 +0000

    перенёс contextMenu + minor cleanup

--- src/hackday/viside/fragments/BlockListFragment.java
+++ src/hackday/viside/fragments/BlockListFragment.java
@@ -1,12 +1,13 @@
 package hackday.viside.fragments;
 
+import android.view.*;
+import android.widget.AdapterView;
+import hackday.viside.blocks.ReceiveMessageBlock;
+import hackday.viside.blocks.SendMessageBlock;
 import hackday.viside.fragments.BlockListFragment.BlockThumb.TYPE;
 import android.app.ListFragment;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
@@ -27,17 +28,62 @@ public class BlockListFragment extends ListFragment
     List<BlockThumb> mList = new LinkedList<BlockThumb>();
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
         super.onActivityCreated(savedInstanceState);
 
         populateList();
 
         BlocksAdapter ad = new BlocksAdapter();
         setListAdapter(ad);
+
+        getListView().setOnCreateContextMenuListener(new View.OnCreateContextMenuListener()
+        {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v,
+                                            ContextMenu.ContextMenuInfo menuInfo) {
+                menu.add(0, 1, 0, "START");
+                menu.add(0, 2, 1, "LEFT");
+                menu.add(0, 3, 3, "RIGHT");
+            }
+        });
     }
 
-    private void populateList()
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.block_list, null);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem aItem)
     {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) aItem.getMenuInfo();
+        BlockThumb thumb = (BlockThumb) getListView().getAdapter().getItem(info.position);
+        String msg = "";
+        switch (aItem.getItemId()) {
+            case 1:
+                msg = "START";
+                break;
+            case 2:
+                msg = "LEFT";
+                break;
+            case 3:
+                msg = "RIGHT";
+                break;
+        }
+
+        if (thumb.type == TYPE.RECEIVE) {
+            ReceiveMessageBlock rec = new ReceiveMessageBlock(getActivity(), msg);
+        } else if (thumb.type == TYPE.SEND) {
+            SendMessageBlock send = new SendMessageBlock(getActivity(), msg);
+        }
+
+        return false;
+    }
+
+
+    private void populateList() {
         BlockThumb receive = new BlockThumb();
         receive.color = Color.parseColor("#ffb264");
         receive.name = "Получить сообщение";
@@ -74,12 +120,6 @@ public class BlockListFragment extends ListFragment
         mList.add(rotate);
         mList.add(move);
         mList.add(time);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.block_list, null);
     }
 
 
